87f653c1a2d3b6519e6a18398409050f2fd67568
kennie-create <kennieboulin1989@gmail.com>
2020-07-18 23:58:28 -0400

    Made modifications to README file

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+jest.mock("global.fetch")
+
+fetch.get.mockResolvedValue({data:[{mission_name: "Launch"},{mission_name: "Rocket"}]})
+
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText("List of SpaceX Missions :");
